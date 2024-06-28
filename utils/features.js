66261dc81d45cd2d383fc8d869d9ea7ba18ae532
github_username <you@example.com>
2024-06-28 17:32:02 +0530

    new commit

--- utils/features.js
+++ utils/features.js
@@ -5,12 +5,13 @@ export const sendCookie = (user,res,statusCode,message)=>{
         _id:user[0]._id
     },process.env.JWT_SECRET_KEY)
     
-    res.cookie("token",token,{
-        httpOnly:true,
-        maxAge: 15 * 60 *1000,
-        SameSite:process.env.NODE_ENV === "Development"? "lax":None,
-        Secure:process.env.NODE_ENV === "Development"? false:true,
+    res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 15 * 60 * 1000,
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "None",
+        secure: process.env.NODE_ENV !== "Development"
     })
+    
 
     res.status(statusCode).json({
         success:true,
